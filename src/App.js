ffb9921802c931b635f6b58781ccb2446bbfeedd
Lim Chee Kin <limcheekin@vobject.com>
2021-08-05 17:18:48 +0800

    feat: added youtube-api-search dependency and first import of search bar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import SearchBar from "./components/search_bar";
+const API_KEY = "AIzaSyBwwPugM54Rs7G6ugyqqnWMLGepjH1WkHE";
+
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  );
 }
 
 export default App;
